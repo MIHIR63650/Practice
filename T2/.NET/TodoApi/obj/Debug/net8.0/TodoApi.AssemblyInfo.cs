89e0d58f5135b5e8e09538a7c460a0863dfb5072
MIHIR63650 <mihir.savla@zeuslearning.com>
2024-08-07 12:17:16 +0530

    minor fixes

--- T2/.NET/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ T2/.NET/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb12dbce237cf2eae8294f035e540f880bd5196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f461bea59584612aa773f1b79282375d8d652e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- dotnet-task/obj/Debug/net8.0/dotnet-task.AssemblyInfo.cs
+++ dotnet-task/obj/Debug/net8.0/dotnet-task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb12dbce237cf2eae8294f035e540f880bd5196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f461bea59584612aa773f1b79282375d8d652e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
